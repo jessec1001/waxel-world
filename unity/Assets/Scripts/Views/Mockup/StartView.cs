e3613fa9ebd7f0efd5c0e9a96f8ce4483011dc3f
jessec1001 <jessecorralejr@gmail.com>
2022-03-11 04:20:12 +0100

    Added animations for all characters
    - Added plugin to easily create animated sprites with .aseprite files
    - Added new animated sprites
    - Adjusted building images accordingly

--- unity/Assets/Scripts/Views/Mockup/StartView.cs
+++ unity/Assets/Scripts/Views/Mockup/StartView.cs
@@ -8,18 +8,13 @@ public class _StartView : GameObjectHandler
     //TODO - Adjust to really login, for now just opens the map szene
     public void LoginWaxCloudWallet()
     {
-        GameObjectHandler.OpenScene("_MapScene");
+        GameObjectHandler.OpenScene("MapScene");
     }
 
     //TODO - Adjust to really login, for now just opens the map szene
     public void LoginAnchorWallet()
     {
-        GameObjectHandler.OpenScene("_MapScene");
-    }
-
-    public void OpenCreateWallet()
-    {
-        Application.OpenURL("https://waxel.net/set-up-wax-wallet/");
+        GameObjectHandler.OpenScene("MapScene");
     }
 
 }
